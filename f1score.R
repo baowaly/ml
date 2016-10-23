4d5c3b9f36ff446748e68964cfa7e65d33b618ba
baowaly <baowaly@gmail.com>
2016-10-23 12:10:58 +0800

    first commit

---
+++ f1score.R
@@ -0,0 +1,58 @@
+# load libraries
+library(caret)
+library(mlbench)
+# load the dataset
+db<-get(data(PimaIndiansDiabetes))
+
+# create a list of 80% of the rows in the original dataset we can use for training
+validation_index <- createDataPartition(PimaIndiansDiabetes$diabetes, p=0.80, list=FALSE)
+# select 20% of the data for validation
+validation <- PimaIndiansDiabetes[-validation_index,]
+# use the remaining 80% of data to training and testing the models
+dataTrain <- PimaIndiansDiabetes[validation_index,]
+
+# prepare resampling method
+control <- trainControl(method="cv", number=5)
+set.seed(7)
+fit <- train(diabetes~., data=dataTrain, method="glm", metric="Accuracy", trControl=control)
+# display results
+#print(fit)
+trainingAccuracy <- fit$results$Accuracy
+print(paste0("Training Accuracy: ", trainingAccuracy))
+
+
+# estimate skill of the validation dataset
+predictions <- predict(fit, validation)
+testResult <- confusionMatrix(predictions, validation$diabetes)
+#print(testResult)
+
+################Built-in Method###########################
+testAccuracy <- testResult$overall["Accuracy"]
+precision <- testResult$byClass['Pos Pred Value']
+recall <- testResult$byClass['Sensitivity']
+f1score <- 2 * ((precision * recall) / (precision + recall))
+
+print(paste0("Test Accuracy: ", testAccuracy))
+print(paste0("Precision: ", precision))
+print(paste0("Recall: ", recall))
+print(paste0("F1score: ", f1score))
+#########################################################
+
+###############Your Method###########################
+result.compare <- table(validation$diabetes, predictions)
+## predict TRUE actual TRUE
+tp <- result.compare[4]
+## predict TRUE actual FALSE
+fp <- result.compare[3]
+## predict FALSE actual TRUE
+fn <- result.compare[2]
+## predict FALSE actual FALSE
+tn <- result.compare[1]
+
+precision <- tp / (tp+fp)
+recall <- tp / (tp+fn)
+f.score <- (2*precision*recall)/(precision+recall)
+print(paste0("Precision: ", precision))
+print(paste0("Recall: ", recall))
+print(paste0("F1score: ", f.score))
+######################################################
