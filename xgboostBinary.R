09bf047e6b3b8322672d6e6bd5019989ddd8581b
Mrinal Kanti Baowaly <baowaly@users.noreply.github.com>
2016-10-25 21:31:47 +0800

    update comment

--- xgboostBinary.R
+++ xgboostBinary.R
@@ -6,16 +6,18 @@ library(xgboost)
 data(PimaIndiansDiabetes)
 
 set.seed(1)
+
+#split the dataset
 train_index <- createDataPartition(PimaIndiansDiabetes$diabetes, p=0.80, list=FALSE)
 dataTrain <- PimaIndiansDiabetes[train_index,]
 dataTest <- PimaIndiansDiabetes[-train_index,]
 
+#prepare the dataset for xgboost
 xgbTrain <- xgb.DMatrix(data.matrix(dataTrain[,1:8]), label=(dataTrain$diabetes=="pos"))
 xgbTest <- xgb.DMatrix(data.matrix(dataTest[,1:8]), label=(dataTest$diabetes=="pos"))
 
 #train the model
 param <- list(booster = "gblinear", max.depth = 5, eta = 1, eval_metric = "error", objective = "binary:logistic")
-
 bst <- xgb.train(param, data = xgbTrain, nthread = 2, nround = 2, watchlist)
 
 #predic
