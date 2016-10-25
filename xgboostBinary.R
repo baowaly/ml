2f45b5f525395df42ae84914b481f1fce7ac4f28
Mrinal Kanti Baowaly <baowaly@users.noreply.github.com>
2016-10-25 21:23:49 +0800

    Simple xgboost Binary Classification

---
+++ xgboostBinary.R
@@ -0,0 +1,30 @@
+# load libraries
+library(caret)
+library(mlbench)
+library(xgboost)
+
+data(PimaIndiansDiabetes)
+
+set.seed(1)
+train_index <- createDataPartition(PimaIndiansDiabetes$diabetes, p=0.80, list=FALSE)
+dataTrain <- PimaIndiansDiabetes[train_index,]
+dataTest <- PimaIndiansDiabetes[-train_index,]
+
+xgbTrain <- xgb.DMatrix(data.matrix(dataTrain[,1:8]), label=(dataTrain$diabetes=="pos"))
+xgbTest <- xgb.DMatrix(data.matrix(dataTest[,1:8]), label=(dataTest$diabetes=="pos"))
+
+#train the model
+param <- list(booster = "gblinear", max.depth = 5, eta = 1, eval_metric = "error", objective = "binary:logistic")
+
+bst <- xgb.train(param, data = xgbTrain, nthread = 2, nround = 2, watchlist)
+
+#predic
+pred <- predict(bst, xgbTest)
+
+#make binary prediction 
+prediction <- as.numeric(pred > 0.5)
+
+#find the accuracy
+target <- as.numeric((dataTest$diabetes=="pos"))
+test.confMatrx <- confusionMatrix(data=prediction, reference=target, positive="1", mode = "prec_recall")
+print(test.confMatrx)
